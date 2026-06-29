95506877c8c91192fac95872291934c062801af8
“Ire <2960424o@student.gla.ac.uk>
2026-06-29 19:32:37 +0100

    Updated NavBar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,12 +3,12 @@
 export default function Dashboard(){
 return(
     <>
-    <div className="grid grid-col-1 gap-11 p-12 bg-pink-400 w-40 h-200 items-center text-lg" >
-        <a href="#" className="bg-pink-200 h-20 w-25 text-center pt-5">Home</a>
-        <a className="">All Products</a>
-        <a className="">About</a>
-        <a className="">Support</a>
-        <a className="">Blog</a>
+    <div className="grid grid-col-1 gap-11 p-12 bg-pink-400 w-30 h-200 items-center text-lg pr-60">
+        <a href="#" className="bg-white text-pink-600 hover:bg-gray-100 border-4 border-pink-200  shadow-lg shadow-black/40 hover:shadow-xl hover:shadow-black/50 transition-all duration-300 focus:ring-4 focus:ring-pink-300 focus:ring-offset-1 hover:-translate-y-1   text-pink-400   h-16 w-55 text-center  pt-4 rounded-xl">Home</a>
+        <a href="#" className="bg-white text-pink-600 hover:bg-gray-100 border-4 border-pink-200  shadow-lg shadow-black/40 hover:shadow-xl hover:shadow-black/50 transition-all duration-300 focus:ring-4 focus:ring-pink-300 focus:ring-offset-1 hover:-translate-y-1   text-pink-400   h-16 w-55 text-center  pt-4 rounded-xl">All Products</a>
+        <a href="#" className="bg-white text-pink-600 hover:bg-gray-100 border-4 border-pink-200  shadow-lg shadow-black/40 hover:shadow-xl hover:shadow-black/50 transition-all duration-300 focus:ring-4 focus:ring-pink-300 focus:ring-offset-1 hover:-translate-y-1   text-pink-400   h-16 w-55 text-center  pt-4 rounded-xl">About</a>
+        <a href="#" className="bg-white text-pink-600 hover:bg-gray-100 border-4 border-pink-200  shadow-lg shadow-black/40 hover:shadow-xl hover:shadow-black/50 transition-all duration-300 focus:ring-4 focus:ring-pink-300 focus:ring-offset-1 hover:-translate-y-1   text-pink-400   h-16 w-55 text-center  pt-4 rounded-xl">Support</a>
+        <a href="#" className="bg-white text-pink-600 hover:bg-gray-100 border-4 border-pink-200  shadow-lg shadow-black/40 hover:shadow-xl hover:shadow-black/50 transition-all duration-300 focus:ring-4 focus:ring-pink-300 focus:ring-offset-1 hover:-translate-y-1   text-pink-400   h-16 w-55 text-center  pt-4 rounded-xl">Blog</a>
     </div>
     
     </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,9 +5,11 @@ function NavBar(){
 
 return(
     <>
-    <div className="flex justify-center items-center bg-blue-200 h-20">
-        <img src="#"/>
-        <input type="text" />
+    <div className="flex justify-center items-center bg-blue-200 h-30">
+        <div className="mr-160 flex justify-center items-center w-400">
+        <img src="src/assets/CakeLogo.webp" className="w-35 h-30 mix-blend-multiply"/><p className="ml-1 text-3xl text-sky-900 ">Cake Shop</p>
+        </div>
+        <input type="text"className="bg-white rounded-xl w-300 h-8 mr-10 border-3 border-blue-500 ring-3 ring-offset-2 ring-blue-400 text-lg" /><img src=""/>
         <div>
             
         </div>
