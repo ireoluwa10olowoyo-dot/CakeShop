25308ad663df3cfb6b2b24e6d3e8b86c75403fc7
“Ire <2960424o@student.gla.ac.uk>
2026-07-23 00:23:49 +0100

    Added New links to NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,23 @@
-
+import { Link, NavLink } from 'react-router-dom';
 
 
 function NavBar(){
 
 return(
     <>
-    <div className=" z-50 flex h-30 items-center bg-pink-100 px-6">
-        <div className="mr-160 flex justify-center items-center w-400 relative">
+    <div className=" z-50 flex h-30 items-center bg-pink-100 gap-60 w-254.2">
+        <div className="mr-0 flex justify-center items-center w-400 relative">
         <img src="src/assets/CakeLogo.webp" className="w-35 h-30 mix-blend-multiply"/><p className="ml-1 text-3xl text-sky-900 ">Cake Shop</p>
         </div>
-        <input type="text"className="bg-white rounded-xl w-300 h-8 border-3 border-pink-500 ring-3 ring-offset-2 ring-pink-400 text-lg" /><img className="absolute right-1  w-7 h-7 mix-blend-multiply" src="src/assets/Search.webp"/>
-        <div>
-            
+         <div className="flex items-center  justify-center ">
+            <NavLink to="/Home" className="text-pink-900 mr-10 text-lg ">Home</NavLink>
+            <NavLink to="/AllProducts" className="text-pink-900  w-30 text-lg ">All Products</NavLink>
+            <NavLink to="/About" className="text-pink-900  mr-10 text-lg ">About</NavLink>
+            <NavLink to="/Support" className="text-pink-900  mr-10 text-lg ">Support</NavLink>
+            <NavLink to="/Blog" className="text-pink-900 text-lg">Blog</NavLink>
         </div>
+        <input type="text"className="bg-white rounded-xl mr-10 w-285 h-8 border-3 border-pink-500 ring-3 ring-offset-2 ring-pink-400 text-lg" /><img className="absolute right-1  w-7 h-7 mix-blend-multiply" src="src/assets/Search.webp"/>
+       
     </div>
     </>
 )
