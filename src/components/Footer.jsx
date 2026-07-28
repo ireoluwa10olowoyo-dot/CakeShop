363dd6c08b97e832979fd81527536a8b3e900ecb
“Ire <2960424o@student.gla.ac.uk>
2026-07-28 23:46:37 +0100

    Chnaged Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,35 +4,43 @@ import { Link, NavLink } from 'react-router-dom';
 function Footer(){
 return(
     <>
-    <footer class="flex flex-col space-y-10 justify-center m-10 ">
-
-    <nav class="flex justify-center flex-wrap gap-6 text-gray-500 font-medium">
-        <NavLink to="/Home" className="hover:text-gray-900">Home</NavLink>
-        <NavLink to="/AllProducts" className="hover:text-gray-900">All Products</NavLink>
-        <NavLink to="/About" className="hover:text-gray-900 ">About</NavLink>
-        <NavLink to="/Support" className="hover:text-gray-900">Support</NavLink>
-        <NavLink to="/Blog" className="hover:text-gray-900">Blog</NavLink>
-    </nav>
-
-    <div class="flex justify-left space-x-5">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/30/000000/facebook-new.png" />
-        </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/30/000000/linkedin-2.png" />
-        </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/30/000000/instagram-new.png" />
-        </a>
-        <a href="https://messenger.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/30/000000/facebook-messenger--v2.png" />
-        </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/30/000000/twitter.png" />
-        </a>
+    <div class="bg-rose-50">
+    <div class="max-w-screen-lg px-4 sm:px-6 text-gray-800 sm:grid md:grid-cols-4 sm:grid-cols-2 mx-auto">
+        <div class="p-5 ">
+            <h3 class=" relative z-10 font-bold text-xl text-indigo-600">Company Name</h3>
+        </div>
+        <div class="p-5">
+            <div class="relative z-10 text-sm uppercase text-indigo-600 font-bold">Resources</div>
+            <a class="relative z-10 my-3 block" href="/#">Documentation <span class="text-teal-600 text-xs p-1"></span></a><a
+                class="relative z-10 my-3 block" href="/#">Tutorials <span class="text-teal-600 text-xs p-1"></span></a><a
+                class="relative z-10 my-3 block" href="/#">Support <span class="text-teal-600 text-xs p-1">New</span></a>
+        </div>
+        <div class="p-5">
+            <div class="text-sm uppercase text-indigo-600 font-bold">Support</div>
+            <a class=" relative z-10 my-3 block" href="/#">Help Center <span class="text-teal-600 text-xs p-1"></span></a><a
+                class="relative z-10 my-3 block" href="/#">Privacy Policy <span class="text-teal-600 text-xs p-1"></span></a><a
+                class="relative z-10 my-3 block" href="/#">Conditions <span class="text-teal-600 text-xs p-1"></span></a>
+        </div>
+        <div class="p-5">
+            <div class="text-sm uppercase text-indigo-600 font-bold">Contact us</div>
+            <a class="relative z-10 my-3 block" href="/#">XXX XXXX, Floor 4 San Francisco, CA
+                <span class="relative z-10 text-teal-600 text-xs p-1"></span></a><a class="my-3 block" href="/#">contact@company.com
+                <span class="relative z-10 text-teal-600 text-xs p-1"></span></a>
+        </div>
     </div>
-    <p class="text-center text-gray-700 font-medium">&copy; 2026 Company Ltd. All rights reservered.</p>
-</footer>
+</div>
+
+<div class=" pt-2">
+    <div class="flex pb-5 px-3 m-auto pt-5 border-t text-gray-800 text-sm flex-col
+      max-w-screen-lg items-center">
+        <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
+            
+        </div>
+        <div class=" relative z-10 my-5">© Copyright 2020. All Rights Reserved.</div>
+    </div>
+</div>
+
+
     </>
 )
 }
